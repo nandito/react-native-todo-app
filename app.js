40f39509f55a649368543b8113d43ee988fbdc5d
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-03-03 14:50:57 +0100

    Add active item count to the footer

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ class App extends Component {
           />
         </View>
         <Footer
+          count={filterItems('ACTIVE', this.state.items).length}
           filter={this.state.filter}
           onFilter={this.handleFilter}
         />
